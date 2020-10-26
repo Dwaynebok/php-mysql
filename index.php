aeb1ecc274815349a6120f6b4242f192840bed10
Dwaynebok <dwaynebok19@gmail.com>
2020-10-26 15:49:02 +0100

    about to create my form in insert

--- index.php
+++ index.php
@@ -3,3 +3,7 @@ declare(strict_types=1);
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
+
+require_once "Model/DatabaseConnection.php";
+require_once "Model/Student.php";
+require_once "Model/StudentLoader.php";
